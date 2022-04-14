20c010e4db9348b67e7fe04583d38e21d6e7598a
Sunil patel <chemsp@outlook.com>
2022-04-14 10:46:42 -0400

    Add project files.

---
+++ DAL_Layer/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Layer
+{
+    public class Class1
+    {
+        public List<string> GetName_DAL()
+        {
+          
+
+            /// call to DB and populate list
+            var list = new List<string>();
+            return list;
+        }
+    }
+
+    
+}
---
+++ DAL_Layer/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Layer
+{
+    public partial class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
